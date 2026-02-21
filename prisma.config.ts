cc24df0ccce22c95dbf111fd1dbbadee509cae07
Liam Egan <liam@wethecollective.com>
2026-02-21 11:14:25 -0800

    Removing dotenv trigger

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,9 @@
 import path from 'node:path';
 import { defineConfig } from 'prisma/config';
-import { config } from 'https://deno.land/x/dotenv/mod.ts';
 
 // Deno Deploy tunnel provides DATABASE_URL automatically
 // const connectionString = Deno.env.get('DATABASE_URL');
-const connectionString = Deno.env.get('DATABASE_URL')! || config().DATABASE_URL;
+const connectionString = Deno.env.get('DATABASE_URL')!;
 
 if (!connectionString) {
 	console.warn(
